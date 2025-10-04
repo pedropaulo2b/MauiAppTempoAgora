403f8ee8d52371ee5a9ad1b4b4c258ce131b4208
pedro <pedropbb2324@gmail.com>
2025-10-04 06:48:39 -0300

    funcionando

--- MauiAppTempoAgora/Services/DataService.cs
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
 
 namespace MauiAppTempoAgora.Services
 {
-    internal class DataService
+    public class DataService
     {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "394384ccee2e579257c40cc7c5784249";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    }; // Fecha obj do Tempo.
+
+                 
+                
+                
+                } // Fecha if se o status do servidor foi de sucesso
+                else if (resp.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    throw new Exception("Cidade não encontrada. Verifique o nome e tente novamente.");
+                }
+
+            } // fecha laço using
+
+            return t;
+        }
     }
 }
